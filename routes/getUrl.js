84fc47943126177ccf39eeba5d7fc94aa08e43c4
Shivendra <shivendra.verma@jungleworks.com>
2020-11-03 13:12:36 +0530

    Added update and delete functionality

--- routes/getUrl.js
+++ routes/getUrl.js
@@ -3,16 +3,21 @@ const ShortUrl = require('../models/short')
 const router = express.Router()
 
 router.get('/', async function (req, res) {
-    const short = await ShortUrl.find({})
-    res.json(short)
+    try {
+        const short = await ShortUrl.find({})
+        res.json(short)   
+    } catch (error) {
+        res.send(error.message)
+    }
 })
 
-router.get('/:urlHash',async function(req,res){
+router.get('/configurable-shortened-url-domain/:urlHash',async function(req,res){
+    console.log(req.params.urlHash)
     try {
         const shortenedUrl = await ShortUrl.findOne({urlHash:req.params.urlHash});
         return res.json(shortenedUrl)
     } catch (error) {
-        res.send(error)
+        res.send(error.message)
     }
 })
 
--- routes/postUrl.js
+++ routes/postUrl.js
@@ -4,15 +4,43 @@ const router = express.Router()
 const shortid = require('short-uuid')
 
 router.post('/shorturl', async function (req, res) {
-    try {
-        const urlHash = shortid.generate()
-        const shortUrl = "https://configurable-shortened-url-domain"+'/'+urlHash;
-        const createShort = await ShortUrl.create({url: req.body.url,urlHash,shortUrl});
-        res.json(createShort)
+    try 
+    {
+        const alreadyPresent = await ShortUrl.findOne({url: req.body.url}, 'url');
+        if(alreadyPresent == null)
+        {
+            const urlHash = shortid.generate();
+            const shortUrl = "https://configurable-shortened-url-domain"+'/'+urlHash;
+            const createShort = await ShortUrl.create({url: req.body.url,urlHash,shortUrl});
+            res.json(createShort);
+        }
+        else {
+            const createShort = await ShortUrl.findOne({url: req.body.url})
+            res.send(createShort);
+        }
     } catch (error) {
-        res.send(error)
+        res.send(error.message)
     }
 
+})
+
+router.put('/:urlHash', async (req, res) => {
+    try{
+        const updatedUrl = await ShortUrl.findOneAndUpdate({urlHash:req.params.urlHash},{url: req.body.url});
+        return res.redirect('/');
+    }
+    catch (error) {
+        res.send(error.message)
+    }
+})
+
+router.delete('/:urlHash', async (req, res) => {
+    try {
+        const remainingUrl = await ShortUrl.deleteOne({urlHash: req.params.urlHash})
+        return res.redirect('/');
+    } catch (error) {
+        res.send(error.message)
+    }
 })
 
 module.exports = router
